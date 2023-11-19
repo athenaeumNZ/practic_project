f771f420d4899a6cd5709ec80927917fc6ea9687
Tristan Maxwell <103496140+athenaeumNZ@users.noreply.github.com>
2023-11-20 12:18:57 +1300

    Commit #Somthing
    Its gone from gitbub back to me and making another version

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from hello.models import Gender, Person
+
+admin.site.register(Person)
+admin.site.register(Gender)
--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    gender = models.ForeignKey('Gender', on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+class Gender(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
