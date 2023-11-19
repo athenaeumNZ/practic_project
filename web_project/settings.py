ce7def175d524ae0dc0291d0f9799862ada34380
Tristan Maxwell <103496140+athenaeumNZ@users.noreply.github.com>
2023-11-19 16:22:58 +1300

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r%5t()f1!0dz2gfjri7j+_4&k8al53__&1yy5(^9r@#x+677*l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
